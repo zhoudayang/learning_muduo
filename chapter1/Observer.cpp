37a4d20ff7af871b3514478e34c15266f6dbe3c9
Zhou Yang <fec84eb999f0fe7fe071db9082d66cefb7e27ea2>
2016-06-19 20:02:33 +0800

    add code about Observer in 1.8

---
+++ chapter1/Observer.cpp
@@ -0,0 +1,94 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+//契合线程安全的观察者模型写法 -> 充分利用智能指针 
+
+//创建该对象,加锁
+//删除该对象,解锁
+//利用对象及作用域简化调用步骤
+
+class MutexLockGuard {
+public:
+    explicit MutexLockGuard(mutex &mutex_)
+            : mutex_(mutex_) {
+        mutex_.lock();
+    }
+
+    ~MutexLockGuard() {
+        mutex_.unlock();
+    }
+
+private:
+    //使用thread中自带的锁
+    mutex &mutex_;
+};
+
+//前置声明
+class Observable;
+
+class Observer {
+public:
+    Observer(Observable *obj) : obj(obj) { }
+
+    void update() {
+        cout << "get update information" << endl;
+    }
+
+private:
+    Observable *obj;
+};
+
+class Observable {
+private:
+    mutex mutex_;
+    vector<weak_ptr<Observer>> observers_;
+public:
+    void register_(weak_ptr<Observer> x);
+
+    void notifyObservers();
+};
+
+//注册观察者
+void Observable::register_(weak_ptr<Observer> x) {
+    MutexLockGuard lock(mutex_);
+    observers_.push_back(x);
+}
+
+//通知观察者
+void Observable::notifyObservers() {
+    MutexLockGuard lock(mutex_);
+    auto it = observers_.begin();
+    while (it != observers_.end()) {
+        //尝试提升,这一步是线程安全的
+        shared_ptr<Observer> obj(it->lock());
+
+        if (obj) {
+            //提升成功
+            obj->update();
+            //没有race condition,因为obj在栈上,对象不可能在本作用域内销毁
+            ++it;
+        }
+        else {
+
+            //对象已经销毁,从容器中拿掉weak_ptr
+            it = observers_.erase(it);
+        }
+    }
+}
+
+int main() {
+    Observable *observable = new Observable();
+    //此处必须存储这些Observer对象,否则这些对象在离开循环作用域之后将被销毁
+    vector<shared_ptr<Observer>> Observers;
+    for(int i =0;i<5;i++){
+        shared_ptr <Observer >ptr1 (new Observer(observable));
+        weak_ptr<Observer> ptr2(ptr1);
+        Observers.push_back(ptr1);
+        observable->register_(ptr2);
+    }
+    observable->notifyObservers();
+
+    return 0;
+}
