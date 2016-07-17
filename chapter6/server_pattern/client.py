edfdcdb98dee26ebd830198e24370ee3ef902f5c
周炀 <zhoudayang2@163.com>
2016-07-18 00:46:20 +0800

    add note

--- chapter6/server_pattern/client.py
+++ chapter6/server_pattern/client.py
@@ -6,11 +6,12 @@ import threading
 
 
 class myClient(threading.Thread):
+    # 初始化线程
     def __init__(self, address, port):
         threading.Thread.__init__(self)
         self.address = address
         self.port = port
-
+    # 执行线程会调用这个函数
     def run(self):
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         address_info = (self.address, self.port)
@@ -24,7 +25,9 @@ class myClient(threading.Thread):
 
 if __name__ == "__main__":
     thread_list = []
+    # 创建10个threading.Thread对象
     for i in range(10):
         thread_list.append(myClient("127.0.0.1", 2016))
+    # run
     for one_thread in thread_list:
         one_thread.start()
