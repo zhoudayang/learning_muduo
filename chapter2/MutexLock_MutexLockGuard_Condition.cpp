57d1a4a626786277b80971268d08a3ff031c7a92
zhoudayang <zhoudayang2@163.com>
2016-07-28 16:39:16 +0800

    update MutexLock_MutexLockGuard_Condition.cpp

--- chapter2/MutexLock_MutexLockGuard_Condition.cpp
+++ chapter2/MutexLock_MutexLockGuard_Condition.cpp
@@ -6,38 +6,35 @@
 
 using namespace std;
 
-namespace CurrentThread {
-    //存放当前线程的pid_t
-    __thread int t_cachedTid = 0;
-
-    //返回当前线程的pid_t
-    inline int tid();
-}
 namespace detail {
-
+    //返回线程的pid值
     pid_t gettid() {
-        //调用系统函数,得到当前线程的tid
-        return static_cast<pid_t>(::syscall(SYS_gettid));
+        return static_cast<pid_t > (::syscall(SYS_gettid));
     }
 }
-namespace CurrentThread {
-    extern __thread int t_cachedTid;
+namespace CurrentThrerad {
+    __thread int t_cacheTid = 0;
 
-    //缓存tid
     void cacheTid() {
-        t_cachedTid = detail::gettid();
+        t_cacheTid = detail::gettid();
     }
 
-    //返回tid
     inline int tid() {
-        if (__builtin_expect(t_cachedTid == 0, 0)) {
+        /*
+            __builtin_expect() 是 GCC (version >= 2.96）提供给程序员使用的，目的是将“分支转移”的信息提供给编译器，这样编译器可以对代码进行优化，以减少指令跳转带来的性能下降。
+
+            __builtin_expect((x),1) 表示 x 的值为真的可能性更大；
+            __builtin_expect((x),0) 表示 x 的值为假的可能性更大。
+
+              也就是说，使用 likely() ，执行 if 后面的语句 的机会更大，使用 unlikely()，执行 else 后面的语句的机会更大。通过这种方式，编译器在编译过程中，会将可能性更大的代码紧跟着起面的代码，从而减少指令跳转带来的性能上的下降。
+         */
+        //此处t_cachedTid为0的可能性极小(事实上只有第一次调用tid()函数时，cachedTid的值才为0),建议编译器按照这个事实进行编译优化
+        if (__builtin_expect(t_cacheTid == 0, 0)) {
             cacheTid();
         }
-        return t_cachedTid;
+        return t_cacheTid;
     }
-
-
-}
+};
 
 
 class MutexLock {
