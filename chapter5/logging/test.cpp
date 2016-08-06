bba9d6493e3e472424be58676da0ac32917e2d6f
zhoudayang <zhoudayang2@163.com>
2016-08-06 22:44:43 +0800

    change cmakelists.txt

--- chapter5/logging/test.cpp
+++ chapter5/logging/test.cpp
@@ -2,10 +2,15 @@
 // Created by fit on 16-8-6.
 //
 
-#include <iostream>
-using namespace std;
+#include "logging/logging.h"
+
+#include<stdio.h>
 
 int main(){
+    LOG_TRACE << "trace";
+    LOG_DEBUG << "debug";
+    LOG_INFO << "Hello";
+    LOG_WARN << "World";
+    LOG_ERROR << "Error";
 
-    cout<<"hello world"<<endl;
 }
