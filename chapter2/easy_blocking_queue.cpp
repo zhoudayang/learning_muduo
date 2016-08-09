04ac30030a327cb6376469356efda368377ba77e
zhoudayang <zhoudayang2@163.com>
2016-08-09 22:17:06 +0800

    update easy_blocking_queue.cpp

--- chapter2/easy_blocking_queue.cpp
+++ chapter2/easy_blocking_queue.cpp
@@ -1,63 +1,50 @@
-#include<iostream>
-#include<thread>
-#include<queue>
-#include<assert.h>
-#include<unistd.h>
+#include <deque>
+#include <boost/noncopyable.hpp>
+#include <thread>
+#include <condition_variable>
+#include <assert.h>
+#include <unistd.h>
 
-using namespace std;
-//使用条件变量实现的一个简单的阻塞队列
-
-mutex mutex_;
-std::deque<int> queue1;
-condition_variable cond;
-
-int dequeue() {
-    unique_lock<mutex> lock(mutex_);
-    while (queue1.empty()) {
-        cond.wait(lock);
+template <typename T>
+class BlockingQueue:boost::noncopyable {
+public:
+    T dequeue(){
+        std::unique_lock<std::mutex> lock(mutex_);
+        while(queue.empty())
+            cond_.wait(lock);
+        assert(!queue.empty());
+        T top = queue.front();
+        queue.pop_front();
+        return top;
     }
-    assert(!queue1.empty());
-    int top = queue1.front();
-    queue1.pop_front();
-    return top;
-}
-
-void enqueue(int x) {
-    unique_lock<mutex> lock(mutex_);
-    queue1.push_back(x);
-    cond.notify_one();
-}
-
-mutex print_lock;
-
-//线程1中该函数每间隔3秒就向阻塞对列中插入10个数字
-void function1() {
-    while (true) {
-        for (int i = 0; i < 10; i++) {
-            {
-                enqueue(i);
-                lock_guard<mutex> lock(print_lock);
-                cout << "enqueue " << i << endl;
-            }
-
-        }
-        sleep(3);
+    void enqueue(T x){
+        std::unique_lock<std::mutex> lock(mutex_);
+        queue.push_back(x);
+        cond_.notify_one();
     }
-}
-//线程2中该函数不断尝试从阻塞队列中取出元素
-void function2() {
-    while (true) {
-        {
-            lock_guard<mutex> lock(print_lock);
-            cout<<"dequeue and get value ";
-            cout << dequeue() << endl;
-        }
+private:
+    std::mutex mutex_;
+    std::condition_variable cond_;
+    std::deque <T> queue;
+};
+
+
+void enqueue(BlockingQueue<int> &queue){
+    while(true){
+        queue.enqueue(rand());
+        sleep(1);
     }
 }
 
-int main() {
-    thread t1(function1);
-    thread t2(function2);
+void dequeue(BlockingQueue<int> &queue){
+    while(true){
+        printf("get %d from queue now!\n",queue.dequeue());
+    }
+}
+int main(){
+    BlockingQueue<int> queue;
+    std::thread t1(enqueue,std::ref(queue));
+    std::thread t2(dequeue,std::ref(queue));
     t1.join();
     t2.join();
 
