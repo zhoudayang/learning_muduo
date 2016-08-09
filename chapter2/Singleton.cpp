321bb8798df0f792f9ff384cbe2df166a05433d6
zhoudayang <zhoudayang2@163.com>
2016-08-09 22:37:38 +0800

    update Singleton.cpp

--- chapter2/Singleton.cpp
+++ chapter2/Singleton.cpp
@@ -1,9 +1,7 @@
 #include<iostream>
 #include <boost/core/noncopyable.hpp>
 #include <thread>
-
 using namespace std;
-
 //线程安全的Singleton
 template<typename T>
 class Singleton {
@@ -13,66 +11,51 @@ public:
         pthread_once(&pthread_once_, &Singleton::init);
         return *value_;
     }
-
 private:
     Singleton();
-
     ~Singleton();
-
     static void init() {
         value_ = new T();
     }
-
 private:
     static pthread_once_t pthread_once_;
     static T *value_;
 };
-
 //指定静态对象的初始值
 template<typename T>
 pthread_once_t Singleton<T>::pthread_once_ = PTHREAD_ONCE_INIT;
-
+//init value_ with NULL
 template<typename T>
 T *Singleton<T>::value_ = NULL;
-
 class test {
 public:
-    test() : name_("test") { }
-
+    test() : name_("test") ,value_(rand()){ }
     string name() {
-        return name_;
+        return name_+to_string(value_);
     }
-
 private:
     string name_;
+    int value_;
 };
-
-
 int main() {
-    //输出锁
-    mutex print_lock;
 
     thread t1([&]() -> void {
         auto it = Singleton<test>::instance();
         string name = it.name();
-        unique_lock<mutex> lock(print_lock);
-        cout << name << endl;
+        printf("%s\n",name.c_str());
     });
     thread t2([&]() -> void {
         auto it = Singleton<test>::instance();
         string name = it.name();
-        unique_lock<mutex> lock(print_lock);
-        cout << name << endl;
+        printf("%s\n",name.c_str());
     });
     thread t3([&]() -> void {
         auto it = Singleton<test>::instance();
         string name = it.name();
-        unique_lock<mutex> lock(print_lock);
-        cout << name << endl;
+        printf("%s\n",name.c_str());
     });
     t1.join();
     t2.join();
     t3.join();
-
     return 0;
 }
