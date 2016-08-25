6072754ca87f828f756b650b12f042f360eb12f1
zhoudayang <zhoudayang2@163.com>
2016-08-26 02:11:32 +0800

    update copyable.h

--- chapter5/async_log/copyable.h
+++ chapter5/async_log/copyable.h
@@ -4,12 +4,15 @@
 
 #ifndef COPYABLE_H
 #define COPYABLE_H
+
 namespace muduo {
+    
     //a tag class emphasises the objects are copyable
     //the empty base class optimization applies
     //any derived class of copyable should be a value type
     class copyable {
 
     };
+    
 }
 #endif
