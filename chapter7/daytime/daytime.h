b475310c4930f2d3acc1c5d4c7e1319eab14f1ad
zhoudayang <zhoudayang2@163.com>
2016-09-08 22:36:54 +0800

    add some note

--- chapter7/daytime/daytime.h
+++ chapter7/daytime/daytime.h
@@ -10,11 +10,14 @@ class DaytimeServer {
 public:
     DaytimeServer(muduo::net::EventLoop *loop, const muduo::net::InetAddress &listenAddr);
 
+    //start the server
     void start();
 
 private:
+    //connection callback function
     void onConnection(const muduo::net::TcpConnectionPtr &con);
 
+    //message callback function
     void onMessage(const muduo::net::TcpConnectionPtr &conn, muduo::net::Buffer *buf, muduo::Timestamp time);
 
     muduo::net::TcpServer server_;
