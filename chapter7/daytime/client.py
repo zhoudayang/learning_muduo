0266c87f29d85cb0ea8bd631ef7368f6991198d2
zhoudayang <zhoudayang2@163.com>
2016-09-10 01:11:55 +0800

    simplify code

--- chapter7/daytime/client.py
+++ chapter7/daytime/client.py
@@ -3,7 +3,10 @@ import socket
 
 sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 sock.connect(('127.0.0.1',2016))
+# recevie data from server
 data =sock.recv(4096)
 print data
+# send data to server
 sock.send("hello world\n")
+# close socket
 sock.close()
--- chapter7/daytime/main.cpp
+++ chapter7/daytime/main.cpp
@@ -2,9 +2,58 @@
 
 #include <muduo/base/Logging.h>
 #include <muduo/net/EventLoop.h>
+#include<boost/bind.hpp>
+#include <muduo/net/TcpServer.h>
+
+class DaytimeServer {
+public:
+    DaytimeServer(muduo::net::EventLoop *loop, const muduo::net::InetAddress &listenAddr);
+
+    //start the server
+    void start();
+
+private:
+    //connection callback function
+    void onConnection(const muduo::net::TcpConnectionPtr &con);
+
+    //message callback function
+    void onMessage(const muduo::net::TcpConnectionPtr &conn, muduo::net::Buffer *buf, muduo::Timestamp time);
+
+    muduo::net::TcpServer server_;
+};
 
 using namespace muduo;
 using namespace muduo::net;
+
+DaytimeServer::DaytimeServer(muduo::net::EventLoop *loop, const muduo::net::InetAddress &listenAddr)
+        : server_(loop, listenAddr, "DaytimeServer") {
+    server_.setConnectionCallback(
+            boost::bind(&DaytimeServer::onConnection, this, _1)
+    );
+    server_.setMessageCallback(
+            boost::bind(&DaytimeServer::onMessage, this, _1, _2, _3)
+    );
+}
+
+void DaytimeServer::start() {
+    server_.start();
+}
+
+void DaytimeServer::onConnection(const muduo::net::TcpConnectionPtr &con) {
+    LOG_INFO << "DaytimeServer - " << con->peerAddress().toIpPort() << " -> " << con->localAddress().toIpPort() <<
+             " is " << (con->connected() ? "up" : "down");
+    if (con->connected()) {
+        con->send(Timestamp::now().toFormattedString() + "\n");
+        con->shutdown();
+    }
+}
+
+void DaytimeServer::onMessage(const muduo::net::TcpConnectionPtr &conn, muduo::net::Buffer *buf,
+                              muduo::Timestamp time) {
+    string msg(buf->retrieveAllAsString());
+    LOG_INFO << "the client says " << msg;
+    LOG_INFO << conn->name() << " discards " << msg.size() << " bytes received at " << time.toString();
+}
 
 int main() {
     LOG_INFO << "pid = " << getpid();
