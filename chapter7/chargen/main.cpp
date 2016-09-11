c870ab6ded319f7ff95598a23971f797b1485886
zhoudayang <zhoudayang2@163.com>
2016-09-11 22:34:45 +0800

    simplify code and add some note:

--- chapter7/chargen/main.cpp
+++ chapter7/chargen/main.cpp
@@ -1,20 +1,109 @@
-#include "chargen.h"
-
+#include <muduo/net/TcpServer.h>
 #include <muduo/base/Logging.h>
-#include<muduo/net/EventLoop.h>
+#include <muduo/net/EventLoop.h>
+
+#include <boost/bind.hpp>
+#include <stdio.h>
+
+
+class ChargenServer {
+public:
+
+    ChargenServer(muduo::net::EventLoop * loop,const muduo::net::InetAddress &listenAddress, bool print = false);
+
+    void start();
+private:
+
+
+    void onConnection(const muduo::net::TcpConnectionPtr & conn);
+
+    void onMessage(const muduo::net::TcpConnectionPtr &conn,muduo::net::Buffer * buf,muduo::Timestamp time);
+
+    void onWriteComplete( const muduo::net::TcpConnectionPtr &conn);
+
+    void printThroughput();
+
+    muduo::net::TcpServer server_;
+
+    muduo::string message_;
+    int64_t transferred_;
+    muduo::Timestamp startTime_;
+};
 
 using namespace muduo;
 using namespace muduo::net;
 
-//chargen 协议
-//只发送数据，不接收数据，而且发送数据的速度不能快过客户端接收的速度
+ChargenServer ::ChargenServer(muduo::net::EventLoop *loop, const muduo::net::InetAddress &listenAddress, bool print)
+:server_(loop,listenAddress,"ChargenServer"),
+ transferred_(0),
+ startTime_(Timestamp::now())
+{
+    server_.setConnectionCallback(boost::bind(&ChargenServer::onConnection,this,_1));
+    server_.setMessageCallback(boost::bind(&ChargenServer::onMessage,this,_1,_2,_3));
+    server_.setWriteCompleteCallback(boost::bind(&ChargenServer::onWriteComplete,this,_1));
+    //bind function that run every 3 seconds
+    if(print){
+        loop->runEvery(3.0,boost::bind(&ChargenServer::printThroughput,this));
+    }
+    string line;
+    for(int i=33;i<127;i++){
+        line.push_back(char(i));
+    }
+    line += line;
+    for(size_t i=0;i<127-33;i++){
+        message_ += line.substr(i,72);
+        message_+= '\n';
+    }
 
-int main() {
+}
 
-    LOG_INFO << "pid = " << getpid();
+void ChargenServer::start(){
+    server_.start();
+}
+
+void ChargenServer::onMessage(const TcpConnectionPtr & conn,Buffer *buf,Timestamp time){
+    string msg(buf->retrieveAllAsString());
+    LOG_INFO<<conn->name()<<" discards " <<msg.size()<<" bytes received at " <<time.toString();
+}
+
+//call this function every 3 seconds
+void ChargenServer::printThroughput() {
+    Timestamp endTime = Timestamp::now();
+    double time = timeDifference(endTime,startTime_);
+    printf("%4.3f MiB /s \n",static_cast<double>(transferred_/time/1024/1024));
+    //reset transferred_ and startTime_
+    transferred_ =0;
+    startTime_ = endTime;
+}
+
+//function call when build connection
+void ChargenServer::onConnection(const muduo::net::TcpConnectionPtr &conn) {
+    LOG_INFO<<"ChargenServer - "<< conn->peerAddress().toIpPort()<<"->"<<conn->localAddress().toIpPort()<<" is "<< (conn->connected()?"up":"down");
+    if(conn->connected()){
+        conn->setTcpNoDelay(true);
+        //begin to send first message
+        conn->send(message_);
+    }
+}
+
+//when write complete, begin to transfer net message
+//net send loop begin
+void ChargenServer::onWriteComplete(const muduo::net::TcpConnectionPtr &conn) {
+    transferred_ += message_.size();
+    conn->send(message_);
+}
+
+
+
+int main(){
+
+    LOG_INFO<<" pid = "<<getpid();
     EventLoop loop;
-    InetAddress listenAddr(2016);
-    ChargenServer server(&loop, listenAddr, true);
+    InetAddress listenAddr(2019);
+    ChargenServer server(&loop,listenAddr,true);
     server.start();
     loop.loop();
+    return 0;
 }
+//test function:
+//nc localhost 2019
