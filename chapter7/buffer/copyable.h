aa858b2291a2a98ee0e5492c909bb5b10ec20df7
zhoudayang <zhoudayang2@163.com>
2016-09-13 22:55:29 +0800

    add code to buffer

---
+++ chapter7/buffer/copyable.h
@@ -0,0 +1,17 @@
+//
+// Created by zhouyang on 16-9-13.
+//
+
+#ifndef PROJECT_COPYABLE_H
+#define PROJECT_COPYABLE_H
+
+namespace muduo{
+    class copyable{
+
+    };
+}
+
+
+
+
+#endif //PROJECT_COPYABLE_H
